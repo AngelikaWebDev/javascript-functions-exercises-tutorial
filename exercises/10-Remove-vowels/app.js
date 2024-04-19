fbc6b691cf78df59843f4ac53ac9cefba692be06
AngelikaWebDev <110480648+AngelikaWebDev@users.noreply.github.com>
2024-04-20 00:37:23 +0200

    Update app.js

--- exercises/10-Remove-vowels/app.js
+++ exercises/10-Remove-vowels/app.js
@@ -1,6 +1,15 @@
 // Your code goes here
-
-
+const rapid = (str) => {
+    let resultado ="";
+    for(let i = 0; i<str.length; i++) {
+        let  char = str[i].toLowerCase();
+        if ('aeiou'.includes(char)){
+            continue;
+        }
+        resultado += char.toUpperCase();
+    }
+    return resultado
+};
 // Work above this line; do not change code below
 let str = "John";
 console.log(rapid(str));
